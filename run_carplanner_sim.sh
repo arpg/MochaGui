e5dc983273322535d27b81e557dd20630d414fab
Mike M <mikemiles19@gmail.com>
2020-04-21 13:30:27 -0600

    revert to before broke with Hinge2Vehicle

--- run_carplanner_sim.sh
+++ run_carplanner_sim.sh
@@ -1,1 +1,1 @@
-./build/devel/lib/mochagui/gui -params ./learning_params.csv -mesh ./labLoop.ply -logfile /home/mike/code/mochagui/mocha.log -car ./herbie/herbie.blend -wheel ./herbie/wheel.blend
+./build/devel/lib/mochagui/gui -params ./learning_params.csv -mesh ./labLoop.ply -logfile 1.log -car ./herbie/herbie.blend -wheel ./herbie/wheel.blend
